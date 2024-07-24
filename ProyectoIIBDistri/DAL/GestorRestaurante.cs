22eb8e7ef1c917c0455f36177c73caf9443a4b56
Josue267A <97122519+Josue267A@users.noreply.github.com>
2024-07-24 14:52:40 -0500

    string de conexion y DAL

---
+++ ProyectoIIBDistri/DAL/GestorRestaurante.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace ProyectoIIBDistri.DAL
+{
+    public class GestorRestaurante : DbContext
+    {
+        public DbSet<Usuario> Rocas { get; set; }
+        public DbSet<Ticket> Fosiles { get; set; }
+        public DbSet<HistorialTicket> HistorialTickets { get; set; }
+        public DbSet<Comida> Comidas { get; set; }
+    }
+}
