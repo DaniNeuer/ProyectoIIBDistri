0e617b31e8422121b8ecb35b73d15217adcd86fc
DaniAlej <168309704+daniAlej@users.noreply.github.com>
2024-07-25 11:57:57 -0500

    Cambios en el ticket controler

--- ProyectoIIBDistri/Controllers/TicketController.cs
+++ ProyectoIIBDistri/Controllers/TicketController.cs
@@ -2,16 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
+using System.Data.Entity;
+using ProyectoIIBDistri.DAL;
+using ProyectoIIBDistri.Models;
+using System.Web.Http;
+
 
 namespace ProyectoIIBDistri.Controllers
 {
-    public class TicketController : Controller
+    public class TicketController : ApiController
     {
-        // GET: Ticket
-        public ActionResult Index()
-        {
-            return View();
-        }
+        
     }
 }
