ecb76dc56a6d1b0670ea6ccc146ff056a57a83a2
DaniAlej <168309704+daniAlej@users.noreply.github.com>
2024-07-25 11:59:23 -0500

    Creacionde controlador usuario

--- ProyectoIIBDistri/Controllers/UsuarioController.cs
+++ ProyectoIIBDistri/Controllers/UsuarioController.cs
@@ -1,18 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Data.Entity;
-using ProyectoIIBDistri.DAL;
-using ProyectoIIBDistri.Models;
+using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
-
 namespace ProyectoIIBDistri.Controllers
 {
-    public class TicketController : ApiController
+    public class UsuarioController : ApiController
     {
-        
     }
 }
